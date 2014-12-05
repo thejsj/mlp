d748dba61fcf0fe1313a1851a27af95ae35eff59
thejsj <jorge.silva@thejsj.com>
2014-12-05 13:20:17 -0800

    Add test for unauthenticated users

--- server/tests/2-api.js
+++ server/tests/2-api.js
@@ -8,68 +8,83 @@ var _ = require('lodash');
 var fs = require('fs');
 
 describe('API', function () {
-  var user_id;
-  var _promptId = 1;
-  beforeEach(function (done) {
-    request
-      .post({
-        url: 'http://localhost:8000/login',
-        form: {
-          email: 'jorge.silva@thejsj.com',
-          password: 'ilovebrian'
-        }
-      }, function (err, response, body) {
-        user_id = JSON.parse(body).user_id;
-        done();
-      });
-  });
 
-  describe('Prompt', function () {
-    it('should create a prompt', function (done) {
-      var now = moment();
+  describe('Unatheticated Users', function () {
+    it('should return a 401 if unatheticated', function (done) {
       request
-        .post({
-          url: 'http://localhost:8000/api/prompt',
-          form: {
-            title: 'Do the kbtz!',
-            startTime: now.format('x'),
-            endTime: now.add(4, 'h').format('x'),
-            votingEndTime: now.add(6, 'h').format('x'),
-          }
-        }, function (err, response) {
-          expect(response.statusCode).to.equal(200);
+        .get('http://localhost:8000/api/photo', function (err, response, body) {
+          expect(response.statusCode).to.equal(401);
           done();
         });
     });
   });
 
-  describe('Photo', function () {
-    it('should create a photo in a post request', function (done) {
-      var fileContents = fs.readFileSync(__dirname + '/Chicago.png');
-      fs.writeFileSync(__dirname + '/_Chicago.png', fileContents);
+  describe('Authenticated Users', function () {
+
+    var user_id;
+    var _promptId = 1;
+
+    beforeEach(function (done) {
       request
         .post({
-            url: 'http://localhost:8000/api/photo',
+          url: 'http://localhost:8000/login',
+          form: {
+            email: 'jorge.silva@thejsj.com',
+            password: 'ilovebrian'
+          }
+        }, function (err, response, body) {
+          user_id = JSON.parse(body).user_id;
+          done();
+        });
+    });
+
+    describe('Prompt', function () {
+      it('should create a prompt', function (done) {
+        var now = moment();
+        request
+          .post({
+            url: 'http://localhost:8000/api/prompt',
             form: {
-              user_id: 20,
-              prompt_id: 1,
-              image: fs.createReadStream(__dirname + '/_Chicago.png')
+              title: 'Do the kbtz!',
+              startTime: now.format('x'),
+              endTime: now.add(4, 'h').format('x'),
+              votingEndTime: now.add(6, 'h').format('x'),
             }
-          },
-          function (err, response, body) {
+          }, function (err, response) {
             expect(response.statusCode).to.equal(200);
             done();
           });
+      });
     });
 
-    it('should get all photos', function (done) {
-      request
-        .get('http://localhost:8000/api/photo', function (err, response, body) {
-          var result = JSON.parse(body);
-          expect(response.statusCode).to.equal(200);
-          expect(_.last(result).prompt_id).to.equal(_promptId);
-          done();
-        });
+    describe('Photo', function () {
+      it('should create a photo in a post request', function (done) {
+        var fileContents = fs.readFileSync(__dirname + '/Chicago.png');
+        fs.writeFileSync(__dirname + '/_Chicago.png', fileContents);
+        request
+          .post({
+              url: 'http://localhost:8000/api/photo',
+              form: {
+                user_id: 20,
+                prompt_id: 1,
+                image: fs.createReadStream(__dirname + '/_Chicago.png')
+              }
+            },
+            function (err, response, body) {
+              expect(response.statusCode).to.equal(200);
+              done();
+            });
+      });
+
+      it('should get all photos', function (done) {
+        request
+          .get('http://localhost:8000/api/photo', function (err, response, body) {
+            var result = JSON.parse(body);
+            expect(response.statusCode).to.equal(200);
+            expect(_.last(result).prompt_id).to.equal(_promptId);
+            done();
+          });
+      });
     });
   });
 });

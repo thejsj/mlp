00119e4619e6bc18d915fd317290b25666ec8de0
Loring Dodge <loringdodge@yahoo.com>
2014-12-05 12:02:29 -0800

    Fix prompt/prompts controllers and change CSS color scheme

--- client/app/prompts/prompts.js
+++ client/app/prompts/prompts.js
@@ -3,11 +3,13 @@ angular.module("mlp.prompts",['ngFx'])
 .controller("promptsController", function ($scope){
   console.log("I am the prompts controller");
 
-  $scope.prompts = [
+  var dummyPrompts = [
     {title: "Dustin", startTime: '1234', endTime: '1234', duration: '1 hour'},
     {title: "Loring", startTime: '1234', endTime: '1234', duration: '1 hour'},
     {title: "Brian", startTime: '1234', endTime: '1234', duration: '1 hour'},
     {title: "Jorge", startTime: '1234', endTime: '1234', duration: '1 hour'},
   ];
+
+  $scope.prompts = dummyPrompts;
 
 });
